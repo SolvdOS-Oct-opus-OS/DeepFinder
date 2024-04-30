8458b78dcb39eb663dfbc73cbe4a6292975c34bb
Galileo <153758703+Arg0naute@users.noreply.github.com>
2024-04-30 11:39:53 +0200

    Update tools.py
    ajout de searchingDos , une v2 de Searshing la fonction interne de DeepFind,

--- tools.py
+++ tools.py
@@ -6,7 +6,9 @@ deepFind | reçois : un dictionnaire, une liste d'éléments à chercher |retour
 """
 
 
-
+""" 
+-------------------------------------------| La v1 de la fonction qui présente encore des bug |-------------------------------------------------------------
+"""
 def deepFind (Reperatorie,list_to_find):
     def Search (dos,file,list_root=[],root = "",key = ""):
         if root:
@@ -37,3 +39,31 @@ def deepFind (Reperatorie,list_to_find):
 
             lib_root[element] = 'ERROR: FILE NOT FOUND'
     return lib_root
+
+
+
+
+
+
+"""
+-------------------------------------------------| La v2 de la fonction en cours de dev |---------------------------------------------------------------------------
+"""
+def SearshingDos (dos,file,cycle=0,root=''):
+
+    cycle +=1
+    if type (dos) == dict:
+        for dos_in in dos:
+            root_in = root + dos_in+'/'
+            if str(dos_in) == str(file):
+                print(root)
+            #print (dos_in,cycle,root_in)
+            SearshingDos(dos[dos_in],file,cycle,root_in)
+
+    if type(dos) == list:
+        for dos_in in dos:
+            #print (dos_in,cycle,root)
+            if str(dos_in) == str(file):
+                print(root)
+        #print ("")
+
+
