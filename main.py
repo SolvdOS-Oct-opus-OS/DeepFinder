8265eef9874f0cf0a71109df189537ea6f97a632
Galileo <153758703+Arg0naute@users.noreply.github.com>
2024-05-02 13:36:34 +0200

    Update main.py
    Ajout d'une interface provisoire

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import json5
 with open('Repertoir.json5', encoding='UTF-8') as lib:
     dico = json5.load(lib)
 
+word = ''
+while 'stop_run' not in word.lower():
+    word = input('\n |-------------------------------------------------------------'
+                 '\n Entrée |Stop_run| pour arrêter \n \n Que chercher vous ? : ')
+    if 'stop_run' not in word.lower():
+        Input = outword(word, dico)
 
+        for name in Input[0]:
+            if name != 'WORD NOT FOUND':
+                print(SearchingDos(dico, name))
 
+            else:
+                print('WORD NOT FOUND')
 
-liste = ['Thermo','ElectroStat','MagnétoStat','ssfdQDddDdqd.PNG',
-         'Italie.jpg','MontBlancKekette.gif','Tetrice',
-         'Horloge','link_with_windows']
-print(SershingDos(dico,'link_with_windows'))
-print(SearshingWord(liste,"Hor"))
+        if Input[1] == False:
+            print('ATTENTION, Recherche imprecise')
