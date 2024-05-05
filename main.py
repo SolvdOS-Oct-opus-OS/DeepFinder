98035500bc349100d96127d1553ffc1c5898d6f6
Galileo <153758703+Arg0naute@users.noreply.github.com>
2024-05-05 12:46:04 +0200

    Update main.py
    ajout d'un prototype de sélecteur de couleur

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
+import turtle
+from turtle import *
+import sys
+sys.setrecursionlimit(5000)
 from tools import *
+from DevZone import *
 import json5
+from random import *
+
+
 
 with open('Repertoir.json5', encoding='UTF-8') as lib:
     dico = json5.load(lib)
+    DataBase = listingDosFIle(dico)
+    #SearchInBase(dico,DataBase)
+    lib.close()
 
-word = ''
-while 'stop_run' not in word.lower():
-    word = input('\n |-------------------------------------------------------------'
-                 '\n Entrée |Stop_run| pour arrêter \n \n Que chercher vous ? : ')
-    if 'stop_run' not in word.lower():
-        Input = outword(word, dico)
 
-        for name in Input[0]:
-            if name != 'WORD NOT FOUND':
-                print(SearchingDos(dico, name))
+print(SearchingDos(dico,'Driver'))
 
-            else:
-                print('WORD NOT FOUND')
 
-        if Input[1] == False:
-            print('ATTENTION, Recherche imprecise')
+print(SearchingDos(dico,'Driver'))
+turtle.bgcolor('black')
+#drawDos(dico, 80, 120)
+mainloop()
+
+
+with open ('ColorsBase.json5',encoding='UTF-8') as ColorsFile:
+    Colors = json5.load(ColorsFile)
+    ColorsFile.close()
+colorListeUsed = []
+colorListe = []
+for color in Colors['Neon']:
+    colorListe.append(color)
+
+for i in range (100):
+    clr = colorListe[randint(0,len(colorListe)-1)]
+    if clr not in colorListeUsed:
+        colorListeUsed.append(clr)
+        print (clr)
